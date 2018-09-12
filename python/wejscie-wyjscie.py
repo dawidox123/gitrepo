e50c545af8cb471c64df0de1cb5ddb74c0b4ac24
dawidox123 <dawidox123dkwasek@op.pl>
2018-09-12 08:47:38 +0200

    początek wej-wyj

---
+++ python/wejscie-wyjscie.py
@@ -0,0 +1,15 @@
+
+#  
+#  
+
+
+def main(args):
+    a = input("podaj pierwszą liczbe: ")
+    print(a)
+    b = input("podaj drugą liczbę: ")
+    print(b)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
