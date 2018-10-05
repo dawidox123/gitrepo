d71d31aa8d4ffe7dd35ec2f08c9b7590afa57802
dawidox123 <dawidox123dkwasek@op.pl>
2018-10-05 11:40:37 +0200

    dokończenie prostokątów

--- python/figury.py
+++ python/figury.py
@@ -17,15 +17,27 @@ def prostokat1(a, b, znak):
 def prostokat2(a, b, znak):
     for i in range(a):
         for j in range(b):
-            if j == 0 or j == b - 1:
-            print(znak, end='')
+            if j == 0 or j == b - 1 or i == 0 or i == a - 1:
+                print(znak, end='')
+            else:
+                print(" ", end="")
         print()
+
+def choinka(h, znak):
+    pass
+
+# %
+# %%
+# %%%
+# %%%%
+# %%%%%
 
 def main(args):
     a = int(input("podaj pierwszą liczbe: "))
     b = int(input("podaj drugą liczbę: "))
     znak = input("podaj znak")
     prostokat1(a, b, znak)
+    print()
     prostokat2(a, b, znak)
     return 0
 
