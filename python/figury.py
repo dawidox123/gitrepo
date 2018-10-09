bac9be7e94ea8f07ff2aa6e5dd3ce2414ed0acd2
dawidox123 <dawidox123dkwasek@op.pl>
2018-10-09 12:35:06 +0200

    choinka

--- python/figury.py
+++ python/figury.py
@@ -20,25 +20,31 @@ def prostokat2(a, b, znak):
             if j == 0 or j == b - 1 or i == 0 or i == a - 1:
                 print(znak, end='')
             else:
-                print(" ", end="")
+                print("/", end="")
         print()
 
 def choinka(h, znak):
-    pass
+    for i in range(h):
+        if i == 0:
 
-# %
-# %%
-# %%%
-# %%%%
-# %%%%%
+
+
+def choinka2(h, znak):
+
 
 def main(args):
     a = int(input("podaj pierwszą liczbe: "))
     b = int(input("podaj drugą liczbę: "))
     znak = input("podaj znak")
+    h = int(input("podaj wysokosć choinki: "))
     prostokat1(a, b, znak)
     print()
     prostokat2(a, b, znak)
+    print()
+    choinka(h, znak)
+    print()
+    choinka2(h, znak)
+    
     return 0
 
 if __name__ == '__main__':
