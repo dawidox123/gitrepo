81217f7220aad4d2dcbb9f86a634ea46ee0623b6
dawidox123 <dawidox123dkwasek@op.pl>
2018-10-26 11:42:49 +0200

    euklides optymalny

--- cpp/euklides.cpp
+++ cpp/euklides.cpp
@@ -23,16 +23,15 @@ int nwd_klasyczne(int a, int b) {
 }
 
 int nwd_optymalny(int a, int b){
+    int licznik = 0;
     while (a > 0) {
-        if (a > 0) {
-        a = a % b;
+       a = a % b;
         b = b - a;
+         licznik++;
     }
-        else
-        cout << b;
-        }
-        return b;
-    }
+    cout << "powtórzeń: " << licznik << endl;
+    return b;
+}
 
 int main(int argc, char **argv)
 {
@@ -41,10 +40,12 @@ int main(int argc, char **argv)
     a = b = 0;
     cout << "podaj dwie liczby: ";
     cin >> a >> b; 
+    int wynik = nwd_klasyczne(a, b);
     cout << "NWD(" << a << ", " << b << ") = "
-        << nwd_klasyczne(a, b) << endl;
+        << wynik << endl;
+    wynik =   nwd_optymalny(a, b);
     cout << "NWD(" << a << ", " << b << ") = "
-        << nwd_optymalny(a, b) << endl;
+        << wynik << endl;
 	return 0;
 }
 
--- python/euklides_nwd.py
+++ python/euklides_nwd.py
@@ -4,20 +4,30 @@
 #  euklides_nwd.py
 #  
 #  
-#  
+#
+def nwd_optymalne(a, b):
+    while a > 0:
+        a = a % b
+        b = b - a
+    return b
+  
 def nwd_klasyczne(a, b):
+    licznik = 0
     while a != b:
         if a > b:
             a = a - b
         else:
             b = b - a
+        licznik = licznik + 1
+    print("powtórzeń", licznik)
     return a
 
 def main(args):
-   a = int(input("podaj pierwszą liczbe: "))
+    a = int(input("podaj pierwszą liczbe: "))
     b = int(input("podaj drugą liczbe: "))
-    nwd_klasyczne(a, b)
-    print("nww({},{}) = {}".format(a, b, nwd_klasyczne(a, b)))
+
+    print("nwd({},{}) = {}".format(a, b, nwd_klasyczne(a, b)))
+    print("nwd({},{}) = {}".format(a, b, nwd_optymalne(a, b)))
     return 0
 
 if __name__ == '__main__':
