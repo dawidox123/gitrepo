efeb0d8517367489cc41a4a2a5105c6e4e15248b
dawidox123 <dkwasek@op.pl>
2018-11-03 13:02:57 +0100

    minmax

---
+++ python/minmax.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  minmax.py
+#  
+import random
+
+def losuj(ileliczb, maksliczb):
+	liczby = [] 
+	
+	ile = 0 
+	while ile < ileliczb:
+		liczba = random.randint(0, maksliczb)
+		if liczby.count(liczba) == 0:
+			liczby.append(liczba)
+			ile += 1
+	print(liczby)
+	return liczby
+
+def minimum(lista):
+	min = lista[0]
+	for el in lista:
+		if el < min:
+			min = el
+	return min
+	
+def maksimum(lista):
+	max = lista[0]
+	for i, el in enumerate(lista):
+		if el > max:
+			max = el
+	return max	
+#  
+#  
+
+
+def main(args):
+	lista = losuj(10, 50)
+	assert minimum([7, 5, 2, 1, 7]) == 1
+	assert maksimum([7, 5, 2, 1, 7]) == 7
+	print("Min: ", minimum(lista))
+	print("Max: ", maksimum(lista))
+	return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
