82fae313bf9f73bd6fefa24e26d76aba345186c4
dawidox123 <dawidox123dkwasek@op.pl>
2018-11-13 12:35:01 +0100

    anagramy

--- cpp/znaki.cpp
+++ cpp/znaki.cpp
@@ -65,6 +65,8 @@ void ascii(char tab[], int rozmiar){
         kod = (int)tab[i];
         if (kod > 96 && kod < 123)
             cout << (char)(kod-32)<< " ";
+        else if (kod > 64 && kod < 91)
+            cout << (char)(kod+32)<< " ";
         else
             cout << (int)tab[i]<< " ";
     }
