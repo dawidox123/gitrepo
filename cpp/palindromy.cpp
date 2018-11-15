aacd56aceeefb52bd162b5a39fa1d36675465139
dawidox123 <dawidox123dkwasek@op.pl>
2018-11-15 07:55:48 +0100

    palindromy 2

--- cpp/palindromy.cpp
+++ cpp/palindromy.cpp
@@ -8,19 +8,29 @@
 #include <iostream>
 using namespace std;
 
-bool palindrom(char tab[], int roz) {
-    bool czyPal = true;
-	for (int i = 0; i < roz / 2; i++)
-
+bool palindrom(char w[], int r) {
+    bool czy_pal = true;
+    for (int i = 0; i < r / 2; i++) {
+        if (w[i] != w[r - 1 - i]){
+            czy_pal = false;
+            break;
+        }
+    }
+    return czy_pal;
+}
 
 int main(int argc, char **argv) 
 {   
     const int rozmiar = 20;
     char wyraz[20];
-    cout << "podaj wyraz lub zdanie";
+    cout << "podaj wyraz lub zdanie: ";
     cin.getline(wyraz, rozmiar);
     cout << cin.gcount() << endl;
     cout << strlen(wyraz) << endl;
+    if (palindrom(wyraz, strlen(wyraz)))
+        cout << "palindrom";
+    else
+        cout << "nie palindrom";
    
     
     return 0;
