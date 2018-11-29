11e972200709fd3f05766b20d7915a36e3c172ec
dawidox123 <dawidox123dkwasek@op.pl>
2018-11-29 07:55:19 +0100

    horner

---
+++ cpp/horner.cpp
@@ -0,0 +1,33 @@
+/*
+ * horner.cpp
+ * 
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+void drukujw(int stopien, float tbwsp[]) {
+    for (int i; i <= stopien; i++)
+    } 
+
+int main(int argc, char **argv)
+{
+    float x = 0;
+    float *tbwsp;
+    int stopien = 0;
+    cout << "podaj stopień wielomianu: ";
+    cin >> stopien;
+    tbwsp = new float [stopien = 1];
+    cout << tbwsp;
+	for (int i = 0; i <= stopien; i++) {
+        cout << "Podaj wspolczynnik przypotedze " << stopien - i << ": ";
+        cin >> tbwsp[i];
+        
+        }
+        cout << "Podaj argument: ";
+        cin >> x;
+	return 0;
+}
+
