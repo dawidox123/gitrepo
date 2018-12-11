aa0bbb9ba96d3a17b2d8e77ae292711c15980468
dawidox123 <dawidox123dkwasek@op.pl>
2018-12-11 12:35:43 +0100

    fibonacci_re

---
+++ cpp/silnia_re.cpp
@@ -0,0 +1,29 @@
+/*
+ * silnia_re.cpp
+ * 
+ * 
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int silnia_re(int n) {
+    if (n == 0)
+    return 1;
+    return silnia_re(n - 1) * n;
+    
+    }
+
+int main(int argc, char **argv)
+{
+    int n;
+    cout << "podaj liczbę: ";
+    cin >> n;
+    
+    silnia_re(n);
+    cout << silnia_re << endl;
+	return 0;
+}
+
