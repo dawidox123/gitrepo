d3457287afd2a1a46070a94c9c31911049c6c864
dawidox123 <dawidox123dkwasek@op.pl>
2018-12-13 07:55:21 +0100

    sort_buble

--- cpp/sortowanie.cpp
+++ cpp/sortowanie.cpp
@@ -9,12 +9,13 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
-int main(int argc, char **argv)
-{
-	void wypelnij(int tab[], int rozmiar){
-    cout << "wprowadz liczby: " << rozmiar << endl;
+
+  
+void wypelnij(int tab[], int rozmiar){
+    srand(time(NULL));
     for (int i= 0; i < rozmiar; i++) {
-        cin >> tab[i];
+        tab[i]=rand() % 101;
+        //cin >> tab[i];
     }
 }
 
@@ -23,6 +24,36 @@ void drukuj(int tab[], int rozmiar){
         cout << tab[i]<< " ";
     }
 }
+
+void zamien1(int a, int b) {
+    cout << a << " " << b << endl;
+    a=b;
+    b=a;
+    cout << a << " " << b << endl;
+    }
+
+void sort_buble(int tab[], int n) {
+    for (int j = n-1;j > 0;j--) {
+        cout << j <<" ";
+        for (int i = 0;i<j;i++) {
+            if (tab[i] > tab[i+1])
+                
+            }
+        }
+    }
+    
+    
+
+int main(int argc, char **argv)
+{
+    int roz = 20;
+    int tab[roz];
+    wypelnij(tab, roz);
+    cout << endl;
+    drukuj(tab, roz);
+    sort_buble(tab, roz)
+    zamien1(5, 10);
+  
 	return 0;
 }
 
