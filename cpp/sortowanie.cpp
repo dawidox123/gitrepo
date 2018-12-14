a75ee295862f33fa45743b9b701110b9001219a1
dawidox123 <dawidox123dkwasek@op.pl>
2018-12-14 11:40:07 +0100

    sortowanie

--- cpp/sortowanie.cpp
+++ cpp/sortowanie.cpp
@@ -23,21 +23,30 @@ void drukuj(int tab[], int rozmiar){
     for (int i= 0; i < rozmiar; i++) {
         cout << tab[i]<< " ";
     }
+    cout << endl << endl;
 }
 
-void zamien1(int a, int b) {
-    cout << a << " " << b << endl;
+void zamien1(int &a, int &b) {
+    int c = a;
     a=b;
-    b=a;
-    cout << a << " " << b << endl;
+    b=c;
+    //cout << a << " " << b << endl;
+    }
+
+void zamien2(int tab[], int i) {
+    int c = tab[i];
+    tab[i]=tab[i+1];
+    tab[i+1]=c;
+    //cout << a << " " << b << endl;
     }
 
 void sort_buble(int tab[], int n) {
     for (int j = n-1;j > 0;j--) {
-        cout << j <<" ";
+
         for (int i = 0;i<j;i++) {
             if (tab[i] > tab[i+1])
-                
+                //zamien1(tab[i], tab[i+1]);
+                zamien2(tab, i);
             }
         }
     }
@@ -51,9 +60,12 @@ int main(int argc, char **argv)
     wypelnij(tab, roz);
     cout << endl;
     drukuj(tab, roz);
-    sort_buble(tab, roz)
-    zamien1(5, 10);
-  
+    sort_buble(tab, roz);
+    drukuj(tab, roz);
+    cout << endl;
+    //int a = 5; int b = 10;
+    //zamien1(a, b);
+    //cout << a << " " << b << endl;
 	return 0;
 }
 
