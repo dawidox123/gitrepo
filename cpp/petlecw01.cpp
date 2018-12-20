1173bd7051cee7092412fbe491cdc48eb08491c8
dawidox123 <dawidox123dkwasek@op.pl>
2018-12-20 07:55:34 +0100

    cw

---
+++ cpp/petlecw01.cpp
@@ -0,0 +1,44 @@
+/*
+ * petlecw01.cpp
+ * 
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+void cw01() {
+    int i;
+    int suma;
+    for (i = 10;i<100;i++) {
+        if (i%2==0)
+        suma = suma + i;
+        }
+        cout << suma << endl;
+    }
+
+void cw02() {
+    int a, b, suma;
+    cout << "podaj liczbe: ";
+    cout << "podaj drugą: ";
+    cin >> a >> b;
+    suma = a+b;
+    while (a+b=suma) {
+    cout << suma;
+    cout << "poddaj liczbe: ";
+        }
+    }
+    
+void cw03a() {
+    int a;
+    cout << "podaj osiem liczb ";
+    cin >> a;
+    }
+
+int main(int argc, char **argv)
+{
+	cw02();
+	return 0;
+}
+
