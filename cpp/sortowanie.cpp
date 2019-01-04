145a3df8487d79127cb0d291c1b11c6216f1bd5d
dawidox123 <dawidox123dkwasek@op.pl>
2019-01-04 11:41:36 +0100

    insert

--- cpp/sortowanie.cpp
+++ cpp/sortowanie.cpp
@@ -62,7 +62,19 @@ void sort_selection(int tab[], int n) {
             zamien1(tab[i], tab[k]);
         }
     }
-    
+
+void sort_insert(int tab[], int n) {
+    int i, k, el;
+    for (i=1;i<n;i++) {
+        el = tab[i];
+        k = i-1;
+        while (k>=0 && tab[k]>el) {
+            tab[k+1]=tab[k];
+            k--;
+        }
+        tab[k+1]=el;
+    }    
+}
 
 int main(int argc, char **argv)
 {
