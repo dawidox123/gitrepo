959950b911981bfb88540b5f6bc6ac2f63ce115f
dawidox123 <dawidox123dkwasek@op.pl>
2019-01-11 11:41:00 +0100

    wyszukaj

--- cpp/wyszukaj.cpp
+++ cpp/wyszukaj.cpp
@@ -7,12 +7,12 @@
 
 
 #include <iostream>
-
+using namespace std;
   
 void wypelnij(int tab[], int rozmiar){
     srand(time(NULL));
     for (int i= 0; i < rozmiar; i++) {
-        tab[i]=rand() % 10000;
+        tab[i]=rand() % 10;
         //cin >> tab[i];
     }
 }
@@ -43,28 +43,41 @@ int szukaj_lin(int tab[], int roz, int szuk) {
     for (int i = 0;i<roz;i++) {
         if (tab[i] == szuk)
             return i;
-        return -1;
     }
+    return -1;
 }
 
-int wyszukaj_bin(int tab[], int roz, int szuk){
-    
+int wyszukaj_bin_lin(int tab[], int roz, int szuk){
+    int p, k, s;
+    p = -1;
+    k = roz - 1;
+    while (p <= k) {
+        s = (p+k)/2;
+        if (tab[s] == szuk){
+            p = s;
+            break;
+        } else if (szuk<tab[s]) k = s - 1;
+        else p = s + 1;
     }
+    return p;
+}
 
 int main(int argc, char **argv)
 {
-    int roz = 41;
+    int roz = 20;
     int tab[roz];
-    wypelnik(tab, roz);
+    wypelnij(tab, roz);
     drukuj(tab, roz);
     int szuk = 0;
     cout << "Podaj szukany element: ";
     cin >> szuk;
-    if (szukaj_lin(tab, roz, szuk))
-        cout << "znaleziono";
-    else
-        cout << "nie znaleziono";
-	
+    //~if (szukaj_lin(tab, roz, szuk))
+        //~cout << "znaleziono";
+    //~else
+        //~cout << "nie znaleziono";
+	sort_insert(tab, roz);
+    int indeks=wyszukaj_bin_lin(tab, roz, szuk);
+    cout << indeks;
 	return 0;
 }
 
