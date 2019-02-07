2b7eb53e002ae9ceafb08304314270c1ae824b32
dawidox123 <dawidox123dkwasek@op.pl>
2019-02-07 08:44:50 +0100

    binarny

---
+++ cpp/konwersje.cpp
@@ -0,0 +1,34 @@
+/*
+ * konwersje.cpp
+ * 
+ * 
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+void   deck2any(int l. int p) {
+    int r[8];
+    du{
+       r[i] = l%p;
+       l = l/p;
+       i++; 
+        
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int l = 0;
+    int p = 0;
+    cout << "podaj liczbe: ";
+    cin >> l;
+    cout << "podaj podstawe: ";
+    cin >> p;
+    deck2any(l. p);
+	
+	return 0;
+}
+
