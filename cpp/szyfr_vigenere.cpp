fe3e69f14c423b9904ba3316ecffd77b100361a5
dawidox123 <dawidox123dkwasek@op.pl>
2019-02-28 08:07:04 +0100

    s

---
+++ cpp/szyfr_vigenere.cpp
@@ -0,0 +1,24 @@
+/*
+ * szyfr_vigenere.cpp
+ * 
+ * 
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int a;
+    cout << "podaj imie: ";
+    cin >> a;
+    if (a=="dawid")
+    
+    cout << "hejka elkwasinio";
+    cout << "hejka karol";
+	
+	return 0;
+}
+
